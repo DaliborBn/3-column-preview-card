85938ea9c8292cdbd298759057af80d37486abe4
Dalibor Perić <109923493+DaliborBn@users.noreply.github.com>
2023-03-26 14:23:48 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -5,26 +5,24 @@ const btnSedans = document.querySelector('#btn-sedans');
 const btnSuvs = document.querySelector('#btn-suvs');
 const btnLuxury = document.querySelector('#btn-luxury');
 
-btnSedans.addEventListener('mouseout', () => {
+btnSedans.addEventListener('mouseover', () => {
   if (!sedans.classList.contains('animation-1')) {
     sedans.classList.toggle('animation-1');
   } else {
     sedans.classList.toggle('animation-2');
   }
 });
-btnSuvs.addEventListener('mouseout', () => {
+btnSuvs.addEventListener('mouseover', () => {
   if (!suvs.classList.contains('animation-1')) {
     suvs.classList.toggle('animation-1');
   } else {
     suvs.classList.toggle('animation-2');
   }
 });
-btnLuxury.addEventListener('mouseout', () => {
+btnLuxury.addEventListener('mouseover', () => {
   if (!luxury.classList.contains('animation-1')) {
     luxury.classList.toggle('animation-1');
   } else {
     luxury.classList.toggle('animation-2');
   }
 });
-
-
