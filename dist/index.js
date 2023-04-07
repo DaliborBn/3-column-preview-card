2b248537315c4ced7b2938f8aff277ffd1a36028
Dalibor Perić <109923493+DaliborBn@users.noreply.github.com>
2023-04-07 19:28:34 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,22 @@
-const sedans = document.querySelector('#i-sedans');
-const suvs = document.querySelector('#i-suvs');
-const luxury = document.querySelector('#i-luxury');
-const btnSedans = document.querySelector('#btn-sedans');
-const btnSuvs = document.querySelector('#btn-suvs');
-const btnLuxury = document.querySelector('#btn-luxury');
+const btnArray = document.querySelectorAll('.btn');
+const icoArray = document.querySelectorAll('img');
 
-btnSedans.addEventListener('mouseover', () => {
-  if (!sedans.classList.contains('animation-1')) {
-    sedans.classList.toggle('animation-1');
-  } else {
-    sedans.classList.toggle('animation-2');
-  }
+btnArray.forEach((element, index) => {
+  element.addEventListener('mouseover', (event) => {
+    let hoveredElement = event.target;
+    hoveredElement.innerText = 'Hit The Road!';
+    
+    if (!icoArray[index].classList.contains('animation-1')) {
+      icoArray[index].classList.toggle('animation-1');
+    } else {
+      icoArray[index].classList.toggle('animation-2');
+    }
+  });
 });
-btnSuvs.addEventListener('mouseover', () => {
-  if (!suvs.classList.contains('animation-1')) {
-    suvs.classList.toggle('animation-1');
-  } else {
-    suvs.classList.toggle('animation-2');
-  }
-});
-btnLuxury.addEventListener('mouseover', () => {
-  if (!luxury.classList.contains('animation-1')) {
-    luxury.classList.toggle('animation-1');
-  } else {
-    luxury.classList.toggle('animation-2');
-  }
+
+btnArray.forEach((element) => {
+  element.addEventListener('mouseout', (event) => {
+    let hoveredElement = event.target;
+    hoveredElement.innerText = 'Learn more';
+  });
 });
